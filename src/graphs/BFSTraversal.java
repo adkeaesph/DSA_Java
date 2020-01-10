7961df3f9a916fbccfe867f2112cf591752560be
adkeaesph <35371920+adkeaesph@users.noreply.github.com>
2020-01-10 12:56:17 +0530

    bfs traversal corrected

--- src/graphs/BFSTraversal.java
+++ src/graphs/BFSTraversal.java
@@ -2,53 +2,78 @@ package graphs;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class BFSTraversal extends EasyGraph {
 	
-	public static void main(String[] args) {
-		int nodes=4;
-		List<Integer>[] grph=createGraph(nodes);
-		addEdge(grph,0, 1,true); 
-		addEdge(grph,0, 2,true); 
-		addEdge(grph,1, 2,true); 
-		addEdge(grph,2, 0,true); 
-		addEdge(grph,2, 3,true); 
-		addEdge(grph,3, 3,true);
-		
+	//not for disconnected graph
+	public static List<Integer> bfsTraversal(List<Integer>[] grph, int nodes,int startNode) {
+		boolean[] visited = new boolean[nodes];
+		for (int i = 0; i < nodes; i++)
+			visited[i] = false;
+
+		List<Integer> q = new LinkedList<>();
+		visited[startNode] = true;
+		q.add(startNode);
+		//boolean visitedAll;
+		int j = 0;
+		while (j < q.size()) {
+			//visitedAll = true;
+
+			List<Integer> list = grph[q.get(j)];
+			for (Integer node : list) {
+				if (visited[node] == false) {
+					visited[node] = true;
+					q.add(node);
+				}
+			}
+			/*	whether this part minimises time complexity slightly is debatable....
+			for (int i = 0; i < nodes; i++) {
+				if (!visited[i]) {
+					visitedAll = false;
+					break;
+				}
+			}
+			if (visitedAll)
+				break;
+				*/
+			j++;
+		}
+		return q;
+	}
+	
+	
+	public static List<Integer> dfsTraversal(List<Integer>[] grph, int nodes,int startNode){
 		boolean[] visited=new boolean[nodes];
 		for(int i=0;i<nodes;i++)
 			visited[i]=false;
 		
-		List<Integer> q=new LinkedList<>();
-		int startNode=2;
-		visited[startNode]=true;
+		List<Integer> q = new LinkedList<>();
+		visited[startNode] = true;
 		q.add(startNode);
-		boolean visitedAll;
-		int j=0;
-		while(true) {
-			visitedAll=true;
-			
-			List<Integer> list=grph[q.get(j)];
-			for(Integer node:list) {
-				if(visited[node]==false) {
-					visited[node]=true;
-					q.add(node);
-				}
-			}
+		int i = 0;
+		int j;
 		
-			for(int i=0;i<nodes;i++) {
-				if(!visited[i]) {
-					visitedAll=false;
-					break;
-				}
-			}
-			if(visitedAll)
-				break;
-			j++;
+		return q;
+	}
+	public static void main(String[] args) {
+		int nodes = 6;
+		List<Integer>[] grph = createGraph(nodes);
+		addEdge(grph, 2, 0, true);
+		addEdge(grph, 2, 1, true);
+		addEdge(grph, 1, 3, true);
+		addEdge(grph, 3, 0, true);
+		addEdge(grph, 1, 2, true);
+		addEdge(grph, 3, 3, true);
+		
+		List<Integer> q = bfsTraversal(grph, nodes, 2);
+		for (int i = 0; i < q.size(); i++) {
+			System.out.print(q.get(i) + " ");
 		}
-		for(int i=0;i<q.size();i++) {
-			System.out.print(q.get(i)+" ");
+		
+		System.out.println();
+		q = dfsTraversal(grph, nodes, 2);
+		for (int i = 0; i < q.size(); i++) {
+			System.out.print(q.get(i) + " ");
 		}
 	}
 }
